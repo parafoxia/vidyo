3e3c1b8ea89fe0884167218a8eacf314941d3863
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-07-05 18:35:03 +0100

    Create setup files

---
+++ vidyo/__init__.py
@@ -0,0 +1,10 @@
+__productname__ = "vidyo"
+__version__ = "0.0.0"
+__description__ = (
+    "A simple way to get information on YouTube videos, async or otherwise."
+)
+__url__ = "https://github.com/parafoxia/vidyo"
+__docs__ = "https://vidyo.readthedocs.io/en/latest/"
+__author__ = "Ethan Henderson"
+__license__ = "BSD-3-Clause"
+__bugtracker__ = "https://github.com/parafoxia/vidyo/issues"
